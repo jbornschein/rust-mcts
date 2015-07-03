ee576bd8bee2f1ef90b6ffb63a3bbdb2cbe4d6ee
Jorg Bornschein <jb@capsec.org>
2015-07-02 23:36:11 -0400

    Add argument parsing

--- src/bin/2048.rs
+++ src/bin/2048.rs
@@ -1,6 +1,12 @@
 
+extern crate argparse;
 extern crate mcts;
 extern crate time;
+
+use std::process::exit;
+
+use argparse::{ArgumentParser, StoreTrue, Store};
+
 
 use time::now;
 
@@ -8,13 +14,31 @@ use mcts::mcts::{Game, MCTS};
 use mcts::twofortyeight::TwoFortyEight;
 
 fn main() {
-    let ensamble_size = 15;
-    let n_samples = 100;
-    let ms_per_move = 2000;
+    let mut verbose: bool = false;
+    let mut time_per_move: f32 = 1.0;
+    let mut ensemble_size: usize = 10;
+
+    { 
+        let mut ap = ArgumentParser::new();
+        ap.set_description("2048 playing.");
+        ap.refer(&mut verbose)
+            .add_option(&["-v", "--verbose"], StoreTrue,
+            "Be verbose");
+        ap.refer(&mut time_per_move)
+            .add_option(&["--time-per-second", "-t"], Store,
+            "Time budget per move (in seconds)");
+        ap.refer(&mut ensemble_size)
+            .add_option(&["--ensemble_size", "-e"], Store,
+            "Ensemble size.");
+        ap.parse_args_or_exit();
+    }
+
+    let n_samples = 10;
+    let ms_per_move = (time_per_move * 1000.) as i64;
 
     // Create a game and a MCTS solver
     let mut game = TwoFortyEight::new();
-    let mut mcts = MCTS::new(&game, ensamble_size);
+    let mut mcts = MCTS::new(&game, ensemble_size);
 
     loop {
 
