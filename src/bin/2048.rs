1d64975acaebef9c7101ad1bd9c214116c4ecd27
Jorg Bornschein <jb@capsec.org>
2015-07-02 23:09:30 -0400

    2048 with fixed time budged

--- src/bin/2048.rs
+++ src/bin/2048.rs
@@ -1,21 +1,29 @@
 
 extern crate mcts;
+extern crate time;
+
+use time::now;
 
 use mcts::mcts::{Game, MCTS};
 use mcts::twofortyeight::TwoFortyEight;
 
 fn main() {
-    let ensamble_size = 10;
+    let ensamble_size = 15;
     let n_samples = 100;
+    let ms_per_move = 2000;
 
     // Create a game and a MCTS solver
     let mut game = TwoFortyEight::new();
     let mut mcts = MCTS::new(&game, ensamble_size);
 
     loop {
-        mcts.search(n_samples, 1.);
-        let action = mcts.best_action();
 
+        let t0 = time::now();
+        while (time::now()-t0).num_milliseconds() < ms_per_move {
+            mcts.search(n_samples, 1.);
+        };
+
+        let action = mcts.best_action();
         match action {
             Some(action) => {
                 game.make_move(&action);
